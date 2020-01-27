9902a6f5b847c16b5177ac951feb8a901d004926
vladandrei51 <vladandrei51@gmail.com>
2020-01-27 12:47:52 +0200

    - list + details on click

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "./hero";
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroList: Hero;
+
+  selectedHero: Hero;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.heroList = HEROES;
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
